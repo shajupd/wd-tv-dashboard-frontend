71672d57dea50e1827a07d7599e0f3e9e41e9cb4
Shaju PD <shajupd@webdura.tech>
2024-10-03 11:20:48 +0530

    fix: Update baseURL in axios instance

--- src/api/axios.instance.js
+++ src/api/axios.instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://tv-dashboard-api.webdura.info",
+  baseURL: "https://tv-dashboard-api.webdura.info",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
