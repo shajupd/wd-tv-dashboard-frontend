31b6a60872f7ed4da54cc84481bcde00158ee3aa
Shaju PD <shajupd@webdura.tech>
2024-10-30 11:37:12 +0530

    Change in backend

--- src/api/axios.instance.js
+++ src/api/axios.instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://tv-dashboard-api.webdura.info",
+  baseURL: "https://wd-tv-dashboard-api.webdura.info",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
